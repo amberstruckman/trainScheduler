f5c0430e1f402b1ff46a0d1d5b9853eed8e54038
Amber <amber.r.struckman@gmail.com>
2018-09-29 08:51:00 -0700

    working code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # trainScheduler
+
+User enters the train name, destination, first train time and train frequency into a form.
+
+Above the form a table populates with the train name, destination, next train time and minutes til train. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,101 +10,73 @@ firebase.initializeApp(config);
 
 var trains = firebase.database();
 
-trains.ref().on("value", function(snapshot){ 
-  console.log(snapshot.val());
-})
-
-var name = "";
-var destination = "";
-var firstTrainTime = 0;
-var frequency = 0;
-
   $("#submit").on("click", function(event) {
     // Don't refresh the page!
     event.preventDefault();
 
-    name = $("#train-name").val().trim();
+    let name = $("#train-name").val().trim();
     console.log("name" + name);
-    destination = $("#destination-name").val().trim();
+    let destination = $("#destination-name").val().trim();
     console.log("destination: " + destination);
-    firstTrainTime = $("#firstTrain-time").val().trim();
+    let firstTrainTime = $("#firstTrain-time").val().trim();
     console.log("firstTrainTime: " + firstTrainTime);
-    frequency = $("#frequency-rate").val().trim();
+    let frequency = $("#frequency-rate").val().trim();
     console.log("Frequency: " + frequency);
 
-    var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+    let frequencyInMinutes = parseInt(frequency);
+    let minutesPerDay = 60 * 24;
+    let firstTrainArray = firstTrainTime.split(":");
+    let nextTrainTimeInMinutes = parseInt(firstTrainArray[0]) * 60 + parseInt(firstTrainArray[1]);
+    console.log("first train minutes: " + nextTrainTimeInMinutes);
+    let trainTimesInMinutes = [];
+    while (nextTrainTimeInMinutes < minutesPerDay) {
+      trainTimesInMinutes.push(nextTrainTimeInMinutes);
+      nextTrainTimeInMinutes += frequencyInMinutes;
+    }
 
-    //the math for the other values pushed to the database
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-    firebase.database().ref().push({
+    trains.ref().push({
       name: name,
       destination: destination,
-      firstTrainTime: firstTrainTime,
-      frequency: frequency,
-      nextTrainTime: nextTrain,
-      tMinutesTillTrain: tMinutesTillTrain
+      frequency: frequencyInMinutes,
+      times: trainTimesInMinutes
+      
     });
     
-    //firebase.database().ref().push({
-            //name: name,
-        //     destination : destination,
-        //     firstTrainTime : firstTrainTime,
-        //     frequency : frequency,
-        //     nextTrainTime: nextTrain,
-        //     tMinutesTillTrain: tMinutesTillTrain
-
-            
-        // });
+    
     })
 
 
 
-   trains.ref('/trains').on("child_added", function(snapshot) {
+   trains.ref().on("child_added", function(snapshot) {
+    console.log("child added");
+   
+    let train = snapshot.val();
+    console.log("on-child_added--" + JSON.stringify(train));
+    let trainFields = ["name", "destination", "frequency", "times"];
+    let trainRow = document.createElement("tr");
+    for (let i = 0; i < trainFields.length - 1; i++) {
+      let trainElement = document.createElement("td");
+      trainElement.textContent =  train[trainFields[i]];
+      trainRow.appendChild(trainElement);
+    }
+    let currentMoment = moment();
+    let momentMinutes = currentMoment.hour() * 60 + currentMoment.minute();
+    let nextTrainMinutes = 0;
+    let trainTimeIndex = 0;
+    while (nextTrainMinutes < momentMinutes && trainTimeIndex < train.times.length) {
+      nextTrainMinutes = train.times[trainTimeIndex++];
+    }
 
-    // // Log everything that's coming out of snapshot
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().destination);
-    console.log(snapshot.val().firstTrainTime);
-    console.log(snapshot.val().frequency);
+    let minutesTilNext = nextTrainMinutes - momentMinutes;
+    let nextTrainElement = document.createElement("td");
+    nextTrainElement.textContent =currentMoment.add(minutesTilNext, "minutes").format("HH:mm");
+    trainRow.appendChild(nextTrainElement);
 
-    var train = snapshot.val();
-    console.log(train);
-
-    // var name = train.name;
-    // var destination = train.destination;
-    // var firstTrainTime = train.firstTrainTime;
-    // var frequency = train.frequency;
-
-    // var tbl = $("<tr>");
-  //  var attributes = [name, destination, frequency, nextTrainTime, minAway];
-
-    // for(var i = 0; i < 6; i++) {
-    //     tbl.append('<td>' + attributes[i] + '</td>');
-
-    // }
-
-    // $("#row").append(tbl);
-
-    // Handle the errors
+    let tilNextElement = document.createElement("td");
+    tilNextElement.textContent = minutesTilNext;
+    trainRow.appendChild(tilNextElement);
+    $("#train-schedule").append(trainRow);
+ 
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
